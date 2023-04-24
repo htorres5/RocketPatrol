3f20cabdbd3e555f7cc99747cf6f47d13e86f9c0
htorres5 <99515061+htorres5@users.noreply.github.com>
2023-04-24 13:07:12 -0700

    moved from first assignment repo to this new repo

---
+++ src/main.js
@@ -0,0 +1,16 @@
+console.log("uwu")
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+}
+
+let game = new Phaser.Game(config);
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
